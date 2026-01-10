f7f18d0007c16869206aed253f038b5ab1207c04
Ravish73 <Ravirajkumbhar961@gmail.com>
2026-01-10 14:57:45 +0530

    Add files via upload

---
+++ ArrayList5.java
@@ -0,0 +1,18 @@
+package collection.list;
+
+import java.util.ArrayList;
+
+public class ArrayList5
+{
+    public static void main(String[] args)
+    {
+       ArrayList<Integer> al =new ArrayList<>();
+       al.add(100);
+       al.add(200);
+       al.add(300);
+       al.add(400);
+        System.out.println(al);
+
+
+    }
+}
